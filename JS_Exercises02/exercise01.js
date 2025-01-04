135ee11898730257c89255aeec69db7dbd4810de
Raj Mishra <rajmishra.sde@gmail.com>
2025-01-04 21:56:17 +0530

    exercise01 done

---
+++ JS_Exercises02/exercise01.js
@@ -0,0 +1,7 @@
+//Write a function called 'greet' that takes a name parameter and logs a greeting message to the console.
+
+function greet(name){
+    console.log(`Hello ${name}, Happy New Year!!!`);    
+}
+
+greet("Finishers");
